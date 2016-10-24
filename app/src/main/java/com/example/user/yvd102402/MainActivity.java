2968995e4e9865d3031f82c069de8fd5faab4b2a
Shaojung <shaojung@gmail.com>
2016-10-24 11:03:28 +0800

    toggle button

--- app/src/main/java/com/example/user/yvd102402/MainActivity.java
+++ app/src/main/java/com/example/user/yvd102402/MainActivity.java
@@ -6,14 +6,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     Button btn2;
+    ToggleButton tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn2 = (Button) findViewById(R.id.button2);
+        tb = (ToggleButton) findViewById(R.id.toggleButton);
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,10 +27,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d("MYLOG", "Check:" + isChecked);
+            }
+        });
     }
 
     public void click1(View v)
     {
 
+    }
+
+    public void click3(View v)
+    {
+        EditText ed = (EditText) findViewById(R.id.editText);
+        String str = ed.getText().toString();
+        Log.d("MYLOG", str);
     }
 }
